1941f56abf5c01ddfc30f3866f1d9208faead30e
Abel A <abelabrahamx@gmail.com>
2022-05-17 21:56:03 -0500

    reduced into single array

--- src/helpers/inputValues.js
+++ src/helpers/inputValues.js
@@ -1,74 +1,70 @@
 export const inputValues = [
-  [
-    {
-      name: "zero",
-      value: "0",
-    },
-    {
-      name: "one",
-      value: "1",
-    },
-    {
-      name: "two",
-      value: "2",
-    },
-    {
-      name: "three",
-      value: "3",
-    },
-    {
-      name: "four",
-      value: "4",
-    },
-    {
-      name: "five",
-      value: "5",
-    },
-    {
-      name: "six",
-      value: "6",
-    },
-    {
-      name: "seven",
-      value: "7",
-    },
-    {
-      name: "eight",
-      value: "8",
-    },
-    {
-      name: "nine",
-      value: "9",
-    },
-    {
-      name: "dot",
-      value: ".",
-    },
-  ],
-  [
-    {
-      name: "reset",
-      value: "C",
-    },
-    {
-      name: "equal",
-      value: "=",
-    },
-    {
-      name: "plus",
-      value: "+",
-    },
-    {
-      name: "minus",
-      value: "-",
-    },
-    {
-      name: "multiply",
-      value: "x",
-    },
-    {
-      name: "divide",
-      value: "/",
-    },
-  ],
+  {
+    name: 'zero',
+    value: '0',
+  },
+  {
+    name: 'one',
+    value: '1',
+  },
+  {
+    name: 'two',
+    value: '2',
+  },
+  {
+    name: 'three',
+    value: '3',
+  },
+  {
+    name: 'four',
+    value: '4',
+  },
+  {
+    name: 'five',
+    value: '5',
+  },
+  {
+    name: 'six',
+    value: '6',
+  },
+  {
+    name: 'seven',
+    value: '7',
+  },
+  {
+    name: 'eight',
+    value: '8',
+  },
+  {
+    name: 'nine',
+    value: '9',
+  },
+  {
+    name: 'dot',
+    value: '.',
+  },
+  {
+    name: 'reset',
+    value: 'C',
+  },
+  {
+    name: 'equal',
+    value: '=',
+  },
+  {
+    name: 'plus',
+    value: '+',
+  },
+  {
+    name: 'minus',
+    value: '-',
+  },
+  {
+    name: 'multiply',
+    value: 'x',
+  },
+  {
+    name: 'divide',
+    value: '/',
+  },
 ];
